1814684e9d7e9274dc9065e1e5fcf68df8db8bc3
Sibusiso Wanda Ndzala <sw.ndzala@gmail.com>
2025-06-01 13:28:24 +0200

    Removed build file from the gitignore file

---
+++ build/resources/main/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS to_do_db;
+
+USE to_do_db;
+
+CREATE TABLE IF NOT EXISTS tasks(
+    unique_id int PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    created_at VARCHAR(10) NOT NULL,
+    completed_at VARCHAR(10),
+    status VARCHAR(15) NOT NULL
+);
+
+SELECT * FROM tasks;
